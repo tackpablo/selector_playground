39614c7e58249bf3862f1c8bdc3e27394e03afe0
Pablo <tackpablo@gmail.com>
2025-08-14 07:05:17 -0700

    Updated keak script

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
       <head>
         <Script
           id="keak-script"
-          src="https://keakdev.blob.core.windows.net/scripts/a56219c0-db4a-4176-8b15-7cdb47a521d7-20.js"
-          data-cookie="20"
+          src="https://keakdev.blob.core.windows.net/scripts/a56219c0-db4a-4176-8b15-7cdb47a521d7-22.js"
+          data-cookie="22"
         />
         <style
           dangerouslySetInnerHTML={{
