087a7d89a0a6872199e05fc78548b155eba5e116
Pablo <tackpablo@gmail.com>
2025-10-01 12:40:42 -0700

    Added new general script

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,35 +18,10 @@ export default function RootLayout({
       <head>
         <script
           id="keak-general-script"
-          src="http://localhost:3000/api/scripts/4/general/full/script.js"
+          src="https://zzontar2hsjaawcn.public.blob.vercel-storage.com/scripts/domain-4.js"
           type="text/javascript"
           data-domain="4"
         ></script>
-        <style
-          dangerouslySetInnerHTML={{
-            __html: `.async-hide { opacity: 0 !important; background: white !important; }`,
-          }}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(KEAK, K, E, A, K_, H, I, D, E_) {
-                K.className += ' ' + E;
-                H.start = 1 * new Date;
-                H.end = I = function() {
-                  K.className = K.className.replace(RegExp(' ?' + E), '');
-                };
-                (KEAK[A] = KEAK[A] || []).hide = H;
-                setTimeout(function() {
-                  I();
-                  H.end = null;
-                }, K_);
-                H.timeout = K_;
-              })(window, document.documentElement, 'async-hide', 'dataLayer', 800,
-                { 'CONTAINER_ID': true });
-            `,
-          }}
-        />
       </head>
       <body>{children}</body>
     </html>
