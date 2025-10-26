53ea6379c8a99343ff70aeecf5b951afc91fd01d
Pablo <tackpablo@gmail.com>
2025-10-26 09:22:14 -0700

    Updated keak script

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script
-          id="keak-general-script"
-          src="https://3xawjsjkilfsb1o2.public.blob.vercel-storage.com/scripts/domain-4-httpsselector-playground.vercel.app.js"
-          type="text/javascript"
-          data-domain="4"
-        ></script>
+        <script 
+        id="keak-nextjs-script" 
+        src="https://zzontar2hsjaawcn.public.blob.vercel-storage.com/scripts/domain-11-httpsselector-playground.vercel.app.js" 
+        type="text/javascript" 
+        data-domain="11"></script>
       </head>
       <body>{children}</body>
     </html>
